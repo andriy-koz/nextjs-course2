149643044781c2a951bf8c0de3dbd05c76e9d6cc
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2022-08-02 05:30:32 -0300

    SSE

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a29ee5047c8f7081"; }
+/******/ 		__webpack_require__.h = function() { return "442e4e8b48272312"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
